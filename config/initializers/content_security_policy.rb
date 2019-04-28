335603ae3ed1a4a09b802d5c9a23fe6de083044c
和田 充弘 <benzoh.wada@gmail.com>
2019-04-28 13:49:13 +0900

    CSPの設定がいるみたいやがいったん保留。

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -9,7 +9,12 @@
 #   policy.font_src    :self, :https, :data
 #   policy.img_src     :self, :https, :data
 #   policy.object_src  :none
-#   policy.script_src  :self, :https
+#   if Rails.env.development?
+#     policy.script_src :self, :https, :unsafe_eval
+#   else
+#     policy.script_src :self, :https
+#   end
+#   # policy.script_src  :self, :https
 #   policy.style_src   :self, :https
 
 #   # Specify URI for violation reports
