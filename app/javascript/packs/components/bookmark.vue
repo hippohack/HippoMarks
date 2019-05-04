0b915fd9dec8684a9956fd4962485e810e3b81b7
和田 充弘 <benzoh.wada@gmail.com>
2019-05-05 08:27:49 +0900

    フォルダ内のネスト

---
+++ app/javascript/packs/components/bookmark.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <span v-for="nest_item in nest_items" v-bind:key="nest_item.id">
+      <a v-if="!nest_item.folder_id" :href="nest_item.url" class="text-dark dropdown-item"><i class="fa fa-link" aria-hidden="true"></i> {{ nest_item.name }}</a>
+      <a
+        v-if="nest_item.folder_id"
+        href="javascript:void(0)"
+        class="text-dark dropdown-item"
+        :id="'folder_' + nest_item.id">
+        <i class="fa fa-folder-o" aria-hidden="true"></i> {{ nest_item.folder_id }}
+      </a>
+    </span>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      nest_items: {
+        type: Array
+      }
+    }
+  }
+</script>
--- app/javascript/packs/components/bookmarkbar.vue
+++ app/javascript/packs/components/bookmarkbar.vue
@@ -1,8 +1,21 @@
 <template>
   <div class="bookmarkbar d-flex">
     <div class="bookmarkbar__item" v-for="item in items" v-bind:key="item.id">
-      <a v-if="item.folder_id" v-on:click="test" href="javascript:void(0)" class="text-dark"><i class="fa fa-folder-o" aria-hidden="true"></i> {{ item.folder_id }}</a>
-      <a v-if="!item.folder_id" href="javascript:void(0)" class="text-dark"><i class="fa fa-link" aria-hidden="true"></i> {{ item.name }}</a>
+      <a v-if="!item.folder_id" :href="item.url" class="text-dark"><i class="fa fa-link" aria-hidden="true"></i> {{ item.name }}</a>
+      <a
+        v-if="item.folder_id"
+        href="javascript:void(0)"
+        class="text-dark dropdown-toggle"
+        :id="'folder_' + item.id"
+        role="button"
+        data-toggle="dropdown"
+        aria-haspopup="true"
+        aria-expanded="false">
+        <i class="fa fa-folder-o" aria-hidden="true"></i> {{ item.folder_id }}
+      </a>
+      <div class="dropdown-menu" :aria-labelledby="'folder_' + item.id">
+        <bookmark :nest_items="nest_items"></bookmark>
+      </div>
     </div>
   </div>
 </template>
@@ -16,25 +29,41 @@
       margin: 0 10px;
     }
   }
+  .dropdown-toggle::after {
+    display: none;
+  }
 </style>
 
 <script>
+  import Bookmark from './bookmark.vue'
+
   var items = [
     { id: 1, name: "hoge", url: "fuga", folder_id: 1 },
     { id: 2, name: "hoge", url: "fuga", folder_id: 0 },
-    { id: 3, name: "hoge", url: "fuga", folder_id: 0 },
+    { id: 3, name: "hoge", url: "fuga", folder_id: 1 },
+    { id: 4, name: "hoge", url: "fuga", folder_id: 0 },
+  ]
+  var nest_items = [
+    { id: 12, name: "hoge", url: "fuga", folder_id: 0 },
+    { id: 11, name: "hoge", url: "fuga", folder_id: 2 },
+    { id: 14, name: "hoge", url: "fuga", folder_id: 0 },
+    { id: 13, name: "hoge", url: "fuga", folder_id: 2 },
   ]
 
   export default {
     data: function() {
       return {
         items: items,
+        nest_items: nest_items,
       };
     },
     methods: {
       test: function() {
         alert('hoge')
       }
+    },
+    components: {
+      'bookmark': Bookmark,
     }
   }
 </script>
