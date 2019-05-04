508f33045e5147a956d852ecb1e90b56ddd01aef
和田 充弘 <benzoh.wada@gmail.com>
2019-05-04 22:24:07 +0900

    ダミーデータでフォルダ表示。

--- app/javascript/packs/components/bookmarkbar.vue
+++ app/javascript/packs/components/bookmarkbar.vue
@@ -1,11 +1,40 @@
 <template>
-  <div class="bookmarkbar">
-    hoge
+  <div class="bookmarkbar d-flex">
+    <div class="bookmarkbar__item" v-for="item in items" v-bind:key="item.id">
+      <a v-if="item.folder_id" v-on:click="test" href="javascript:void(0)" class="text-dark"><i class="fa fa-folder-o" aria-hidden="true"></i> {{ item.folder_id }}</a>
+      <a v-if="!item.folder_id" href="javascript:void(0)" class="text-dark"><i class="fa fa-link" aria-hidden="true"></i> {{ item.name }}</a>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
   .bookmarkbar {
     background-color: #ccc;
+    padding: 10px;
+
+    &__item {
+      margin: 0 10px;
+    }
   }
 </style>
+
+<script>
+  var items = [
+    { id: 1, name: "hoge", url: "fuga", folder_id: 1 },
+    { id: 2, name: "hoge", url: "fuga", folder_id: 0 },
+    { id: 3, name: "hoge", url: "fuga", folder_id: 0 },
+  ]
+
+  export default {
+    data: function() {
+      return {
+        items: items,
+      };
+    },
+    methods: {
+      test: function() {
+        alert('hoge')
+      }
+    }
+  }
+</script>
--- app/javascript/packs/components/header.vue
+++ app/javascript/packs/components/header.vue
@@ -4,8 +4,8 @@
       <img src="https://getbootstrap.com/docs/4.3/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
       Bookmarks
     </router-link>
-    <a href="#home"><span><i class="fa fa-home" aria-hidden="true"></i></span></a>
-    <form>
+    <a class="px-2 mr-2 text-dark" href="#home"><i class="fa fa-home fa-lg" aria-hidden="true"></i></a>
+    <form class="flex-grow-1 mr-2">
       <input type="search" class="form-control" id="search-input" placeholder="Search...">
     </form>
     <ul class="flex-row navbar-nav ml-auto">
@@ -16,6 +16,7 @@
         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
           <a class="dropdown-item" href="#">アカウント設定</a>
           <a class="dropdown-item" href="#">システム設定</a>
+          <hr class="my-1">
           <a class="dropdown-item" href="#">ログアウト</a>
         </div>
       </li>
@@ -26,6 +27,9 @@
 <style lang="scss" scoped>
   .navbar-nav .dropdown-menu {
     position: absolute;
-    left: -70px;
+    left: -90px;
+  }
+  #search-input {
+    border-radius: 1.5rem;
   }
 </style>
