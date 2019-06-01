a7c0611ad4b3aa7fd1c9aca8dd92953035366e97
和田 充弘 <benzoh.wada@gmail.com>
2019-06-02 08:03:09 +0900

    RSpecの練習。

--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -3,9 +3,47 @@ require 'rails_helper'
 RSpec.describe Bookmark, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
 
+  before do
+    @params = { account_id: 1 }
+  end
+
+  let(:hoge) {
+    {
+      account_id: 2,
+      parent_id: 2
+    }
+  }
+
+  let(:bookmark) { Bookmark.new(params) }
+  let(:params) { { account_id: 2, parent_id: 2, item_type: '' } }
+  subject { bookmark }
+
   it 'is not be valid without item_type' do
-    bookmark = Bookmark.new(item_type: '')
-    expect(bookmark).not_to be_valid
+    # bookmark = Bookmark.new(item_type: '')
+    is_expected.not_to be_valid
+  end
+
+  it '10 - 1 は 9 になること' do
+    expect(10 - 1).to eq 9
+  end
+
+  context 'ほげ' do
+    before do
+      @params.merge!(item_type: 'folder')
+    end
+    it 'ふが' do
+      bookmark = Bookmark.new(hoge.merge(item_type: 'folder'))
+      # bookmark = Bookmark.new(@params.merge(parent_id: 1))
+      expect(bookmark.account_id).to be > 0
+      expect(bookmark.parent_id).to be > 0
+      expect(bookmark.item_type).to eq 'folder'
+    end
+  end
+
+  describe '足し算' do
+    it '1 + 1 は 2 になること' do
+      expect(1 + 1).to eq 2
+    end
   end
 end
 
