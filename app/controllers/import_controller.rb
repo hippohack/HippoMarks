59e3204838a16eaeb63ec82e2c21e0349e818f9d
和田 充弘 <benzoh.wada@gmail.com>
2019-08-18 10:17:02 +0900

    Change: フラッシュ追加

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -4,9 +4,9 @@ class ImportController < ApplicationController
   def index
     if request.method == 'POST'
       if Bookmark.import(params[:import_file], current_account)
+        flash[:notice] = "インポート処理を完了しました"
         redirect_to root_path
       end
     end
   end
-
 end
