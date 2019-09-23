a595f74e95fef190c825b8ec097250afa053988b
和田 充弘 <benzoh.wada@gmail.com>
2019-09-23 22:09:21 +0900

    Change: ナビにプロフィールへのリンク設置準備

--- app/models/account.rb
+++ app/models/account.rb
@@ -7,7 +7,7 @@ class Account < ApplicationRecord
   has_many :folders, dependent: :destroy
   accepts_nested_attributes_for :folders, allow_destroy: true
   has_many :bookmarks, dependent: :destroy
-  has_many :profiles, dependent: :destroy
+  has_one :profile, dependent: :destroy
   has_many :settings, dependent: :destroy
   has_many :share_urls, dependent: :destroy
 
