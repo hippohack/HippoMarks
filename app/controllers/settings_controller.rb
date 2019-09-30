3a812700bad73fe891f04b52d2b20fdacbeb5170
和田 充弘 <benzoh.wada@gmail.com>
2019-10-01 08:34:13 +0900

    Change: システム設定。

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,2 +1,43 @@
 class SettingsController < ApplicationController
+  before_action :set_setting
+
+  def index
+    render :edit
+  end
+
+  def edit
+
+  end
+
+  def update
+    params.fetch(:setting, {}).each do |param|
+      setting = @settings.find_by(key: param[:key])
+      setting.update!(setting_attributes(param))
+    end
+    render :edit
+  end
+
+  private
+
+  def set_setting
+    @settings = current_account.settings
+  end
+
+  def setting_params
+    params.fetch(:setting, {}).permit(
+      :account_id,
+      :key,
+      :value,
+      :lock_version
+    )
+  end
+
+  def setting_attributes(params)
+    params.permit(
+      :account_id,
+      :key,
+      :value,
+      :lock_version
+    )
+  end
 end
