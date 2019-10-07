935d37305f0f70797018921e586cbb9c88da0389
和田 充弘 <benzoh.wada@gmail.com>
2019-10-08 07:53:33 +0900

    Add: last_access_time column.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_05_030206) do
+ActiveRecord::Schema.define(version: 2019_10_07_224944) do
 
   create_table "accounts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2019_10_05_030206) do
     t.text "keyword", limit: 4294967295
     t.text "og_image_url", limit: 4294967295
     t.integer "impressions", default: 0
+    t.datetime "last_access_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
