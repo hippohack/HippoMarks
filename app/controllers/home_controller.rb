2d690aff4ef8736dbdfda9eb300291b399ef76f8
和田 充弘 <benzoh.wada@gmail.com>
2019-10-17 08:13:42 +0900

    Change: カレントのアイコン表示。

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,7 @@ class HomeController < ApplicationController
     @top_folder = current_account.folders.top_folder(current_account.bookmarkbar_folder_id)
     @folders = @top_folder.folders
     @bookmarks = @top_folder.bookmarks
+    @sort_setting = current_account.settings.find_by(key: 'item_sort').value
     # raise
   end
 
