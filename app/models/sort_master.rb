71356968d51cde41da082331603843165ab6cb33
和田 充弘 <benzoh.wada@gmail.com>
2019-10-18 08:13:02 +0900

    Add: sort_master.

---
+++ app/models/sort_master.rb
@@ -0,0 +1,2 @@
+class SortMaster < ApplicationRecord
+end
---
+++ db/migrate/20191017230112_create_sort_masters.rb
@@ -0,0 +1,9 @@
+class CreateSortMasters < ActiveRecord::Migration[5.2]
+  def change
+    create_table :sort_masters do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
