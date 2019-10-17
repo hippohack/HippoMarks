d35f385c771a74d14b3b02c31faf3824927545ea
和田 充弘 <benzoh.wada@gmail.com>
2019-10-18 08:33:18 +0900

    Change: added slug column.

--- db/migrate/20191017230112_create_sort_masters.rb
+++ db/migrate/20191017230112_create_sort_masters.rb
@@ -2,6 +2,7 @@ class CreateSortMasters < ActiveRecord::Migration[5.2]
   def change
     create_table :sort_masters do |t|
       t.string :name
+      t.string :slug
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -123,6 +123,7 @@ ActiveRecord::Schema.define(version: 2019_10_17_230112) do
 
   create_table "sort_masters", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.string "name"
+    t.string "slug"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
