a0d03e7fb4e765d2d4eefda50ba57dce5d3f1e1d
和田 充弘 <benzoh.wada@gmail.com>
2019-10-18 21:59:48 +0900

    Change: sort_numでのソート。

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,12 +5,13 @@ class HomeController < ApplicationController
     @top_folder = current_account.folders.top_folder(current_account.bookmarkbar_folder_id)
     @sort_setting = current_account.settings.find_by(key: 'item_sort').value
 
-    # TODO: setting/item_sortの値で並び替え
     unless @sort_setting == 'optional'
       @folders = @top_folder.folders.order(@sort_setting.to_sym)
       @bookmarks = @top_folder.bookmarks.order(@sort_setting.to_sym)
     else
-      # raise
+      # TODO: sort_numの設定
+      @folders = @top_folder.folders.order(:sort_num)
+      @bookmarks = @top_folder.bookmarks.order(:sort_num)
     end
 
     # raise
