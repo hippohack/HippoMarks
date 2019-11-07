75327b394005269fce6d4c5dbfa10a29b2875b38
和田 充弘 <benzoh.wada@gmail.com>
2019-11-08 08:35:12 +0900

    Change: set each actions.

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -4,8 +4,14 @@ class HistoriesController < ApplicationController
   end
 
   def delete
+    @history = current_account.bookmarks.find(params[:id])
+    @history.update([last_access_time: nil, impressions: 0])
+    redirect_to histories_path
   end
 
   def delete_all
+    @histories = current_account.bookmarks.histories
+    @histories.update_all([last_access_time: nil, impressions: 0])
+    redirect_to histories_path
   end
 end
