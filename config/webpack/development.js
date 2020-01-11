97570eee2f44e7ba1d98a0ae141ff60298a77f6b
和田 充弘 <benzoh.wada@gmail.com>
2020-01-12 08:41:30 +0900

    Change: console.logをドロップする検証。

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,4 +2,40 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
+// >>> drop_console
+// const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+// const minimize = {
+//   minimizer: [
+//     new UglifyJsPlugin({
+//       uglifyOptions: {
+//         compress: { drop_console: true }
+//       }
+//     })
+//   ],
+// }
+
+// environment.config.set('optimization', minimize)
+
+const TerserPlugin = require('terser-webpack-plugin');
+
+const minimize = {
+  minimizer: [
+    new TerserPlugin({
+      // sourceMap: true, // Must be set to true if using source-maps in production
+      terserOptions: {
+        compress: {
+          drop_console: true,
+        },
+      },
+    }),
+  ],
+}
+
+environment.config.set('optimization', minimize)
+
+// console.log({environment})
+// console.log('environment.config.optimization', environment.config.optimization)
+// <<< drop_console
+
 module.exports = environment.toWebpackConfig()
