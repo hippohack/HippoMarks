d9a1bdeffd79b11c197be9a2c388dcf9a6ab794c
和田 充弘 <benzoh.wada@gmail.com>
2020-01-15 08:33:49 +0900

    WIP: 翻訳ファイル作成｜履歴まで

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   # # https://teratail.com/questions/177576
   # protect_from_forgery prepend: true
 
-  before_action :set_loaded_action, :set_js_debug
+  before_action :set_loaded_action, :set_js_debug, :set_locale
 
   def set_loaded_action
     @loaded_action = controller_path + '#' + action_name
@@ -12,6 +12,14 @@ class ApplicationController < ActionController::Base
 
   def set_js_debug
     @js_debug = ENV.fetch('JS_DEBUG_MODE') { false }
+  end
+
+  def set_locale
+    locale_setting = current_account.settings.find_by(key: 'lang').value
+    # TODO: 仮状態
+    I18n.locale = 'ja'
+    # I18n.locale = locale_setting == 'japanese' ? 'ja' : 'en'
+    # raise
   end
 
 end
