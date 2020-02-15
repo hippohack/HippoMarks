beb04af77876c3c4d1ff9cb091f4cfc01c43e34d
和田 充弘 <benzoh.wada@gmail.com>
2020-02-15 14:25:01 +0000

    Change: トップだけでいいはずなので。

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,36 @@
 module ApplicationHelper
+
+  def default_meta_tags
+    {
+      site: 'HippoMarks',
+      title: 'Your bookmark bar anywhere',
+      reverse: true,
+      separator: '|',
+      description: 'A bookmark bar that you can access from anywhere to change your browser.There is no need to repeat export and import anymore.',
+      keywords: 'hippomarks,bookmarks,hippohack',
+      canonical: request.original_url,
+      noindex: !Rails.env.production?,
+      icon: [
+        { href: image_url('favicon.png') },
+        { href: image_url('favicon.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+      ],
+      og: {
+        site_name: 'HippoMarks',
+        title: 'Your bookmark bar anywhere',
+        description: 'A bookmark bar that you can access from anywhere to change your browser.There is no need to repeat export and import anymore.', 
+        type: 'website',
+        url: request.original_url,
+        image: image_url('og.png'),
+        locale: 'ja_JP',
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@hippohack',
+      },
+      fb: {
+        app_id: ''
+      }
+    }
+  end
+  
 end
