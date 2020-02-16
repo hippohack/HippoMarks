f998e272892ad069b91c1f17a656ef5adf7c2fbb
和田 充弘 <benzoh.wada@gmail.com>
2020-02-16 22:54:23 +0000

    Change: 取ってくる数の任意設定。

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -101,7 +101,9 @@ class SettingsController < ApplicationController
       [key: 'lang', value: 'english'],
       [key: 'home_url', value: '/'],
       [key: 'show_many_visits', value: 'true'],
+      [key: 'many_visits_num', value: '10'],
       [key: 'show_history', value: 'true'],
+      [key: 'history_num', value: '10'],
       [key: 'item_sort', value: 'optional'],
       [key: 'show_bookmarklet', value: 'true']
     ]
